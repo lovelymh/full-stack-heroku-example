4cd3cd7ed31241273b95c702cd3f954a87eff99a
mihee <sadrainbow26@naver.com>
2018-06-27 20:11:19 +0900

    hello:

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require('express');
-
+const path = require('path');
 const app = express();
 const port = process.env.PORT || 5000;
+
+//front단의 정적파일 제공을 위해 추가
+app.use(express.static(path.join(__dirname, 'client/build')));
 
 app.get('/api/hello', (req, res) => {
   res.send({ express: 'Hello From Express' });
